7e2ce47f73f03606b9b26966b273bb8d793ad16d
AymurzinaJulia <aymurzinajulia@gmail.com>
2025-10-16 01:57:58 +0500

    Task2 added

---
+++ Tyuiu.AymurzinaJV.Sprint2.Task2.V15.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.AymurzinaJV.Sprint2.Task2.V15.Lib;
+namespace Tyuiu.AymurzinaJV.Sprint2.Task2.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            int y = 3;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }   
+    }
+}
