39960b6e25432cac0efb602076827ccb07624f9c
AymurzinaJulia <aymurzinajulia@gmail.com>
2025-10-16 16:18:04 +0500

    Task3 added

---
+++ Tyuiu.AymurzinaJV.Sprint2.Task3.V2.Test/DataServiceTest.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.AymurzinaJV.Sprint2.Task3.V2.Lib;
+namespace Tyuiu.AymurzinaJV.Sprint2.Task3.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 4;
+            double y = ds.Calculate(x);
+            double wait = -44.654;
+            Assert.AreEqual(wait, y);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = ds.Calculate(x);
+            double wait = 1.5;
+            Assert.AreEqual(wait, y);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            DataService ds = new DataService();
+            double x = -2;
+            double y = ds.Calculate(x);
+            double wait = -53;
+            Assert.AreEqual(wait, y);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            DataService ds = new DataService();
+            double x = -10;
+            double y = ds.Calculate(x);
+            double wait = -110;
+            Assert.AreEqual(wait, y);
+        }
+
+    }
+}
---
+++ Tyuiu.AymurzinaJV.Sprint2.Task3.V2/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.AymurzinaJV.Sprint2.Task3.V2.Lib;
+DataService ds = new DataService();
+
+Console.Title = "Спринт #2 | Выполнила: Аймурзина Ю. В. | РППб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема:Вложенные операторы if - else                                      *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #2                                                              *");
+Console.WriteLine("* Выполнила: Аймурзина Ю. В. | РППб-25-1                                  *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y      *");
+Console.WriteLine("* с использованием вложенных оператор if-else, где пользователь вводит    *");
+Console.WriteLine("* значение переменной X с клавиатуры.                                     *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(" Введите значение переменной X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+double res = ds.Calculate(x);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(res);
