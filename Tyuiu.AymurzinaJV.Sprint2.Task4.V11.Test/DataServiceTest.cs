ff5516c510a4b574cf48f0c2527e05596e8b2a8f
AymurzinaJulia <aymurzinajulia@gmail.com>
2025-10-16 17:01:05 +0500

    Task4 added

---
+++ Tyuiu.AymurzinaJV.Sprint2.Task4.V11.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.AymurzinaJV.Sprint2.Task4.V11.Lib;
+namespace Tyuiu.AymurzinaJV.Sprint2.Task4.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 2;
+            double wait = 25;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 30;
+            double y = -50;
+            double wait = -50;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+        
+    }
+}
---
+++ Tyuiu.AymurzinaJV.Sprint2.Task4.V11/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.AymurzinaJV.Sprint2.Task4.V11.Lib;
+DataService ds = new DataService();
+
+Console.Title = "Спринт #2 | Выполнила: Аймурзина Ю. В. | РППб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Тернарный оператор                                                *");
+Console.WriteLine("* Задание #4                                                              *");
+Console.WriteLine("* Вариант #11                                                             *");
+Console.WriteLine("* Выполнила: Аймурзина Ю. В. | РППб-25-1                                  *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет требуемое значение                *");
+Console.WriteLine("* с использованием тернарного оператора.                                  *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(" Введите значение переменной X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Введите значение переменной Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+
+double res = ds.Calculate(x, y);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(res);
