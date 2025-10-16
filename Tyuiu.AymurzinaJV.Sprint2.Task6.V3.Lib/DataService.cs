2bfcf8133c7035030c4680e24a035817862be80e
AymurzinaJulia <aymurzinajulia@gmail.com>
2025-10-17 01:06:50 +0500

    Task6 added

---
+++ Tyuiu.AymurzinaJV.Sprint2.Task6.V3.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.AymurzinaJV.Sprint2.Task6.V3.Lib
+{
+    public class DataService : ISprint2Task6V3
+    {
+        public string FindDayName(int value)
+        {
+            string res = value switch
+            {
+                1 => "Понедельник",
+                2 => "Вторник",
+                3 => "Среда",
+                4 => "Четверг",
+                5 => "Пятница",
+                6 => "Суббота",
+                7 => "Воскресенье",
+                _ => "Неверный порядковый номер дня недели."
+            };
+
+            return res;
+
+        }
+    }
+}
---
+++ Tyuiu.AymurzinaJV.Sprint2.Task6.V3/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.AymurzinaJV.Sprint2.Task6.V3.Lib;
+DataService ds = new DataService();
+
+Console.Title = "Спринт #2 | Выполнила: Аймурзина Ю. В. | РППб-25-1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Получение результата из switch                                    *");
+Console.WriteLine("* Задание #6                                                              *");
+Console.WriteLine("* Вариант #3                                                             *");
+Console.WriteLine("* Выполнила: Аймурзина Ю. В. | РППб-25-1                                  *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая использует оператор switch,                 *");
+Console.WriteLine("* вычисляет требуемое значение и возвращает результат.                    *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите порядковый номер дня недели: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+
+
+string res = ds.FindDayName(value);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(res);
